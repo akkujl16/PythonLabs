7d72b651bb7b6ba226990be1ece05973a195e270
akku <akkuratnova16@gmail.com>
2026-05-20 13:45:15 +0200

    Add files via upload

---
+++ lab5.1.py
@@ -0,0 +1,36 @@
+﻿from functools import wraps
+
+def cache_decorator(func):
+    cache = {}
+    
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        # Ключ из аргументов
+        key = (args, tuple(sorted(kwargs.items())))
+        
+        # ПроверКА, есть ли результат в кэше
+        if key in cache:
+            print(f"Возвращение закэшированого результат для {func.__name__}{args}")
+            return cache[key]
+        
+        # Вычисление результата и сохранение в кэш
+        print(f"Вычисление {func.__name__}{args} ")
+        result = func(*args, **kwargs)
+        cache[key] = result
+        return result
+    
+    return wrapper
+
+@cache_decorator
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+
+print("Первый вызов fibonacci(10):")
+result1 = fibonacci(10)
+print(f"Результат: {result1}\n")
+
+print("Второй вызов fibonacci(10) должен вернуться из кэша:")
+result2 = fibonacci(10)
+print(f"Результат: {result2}\n")
---
+++ lab5.py
@@ -0,0 +1,47 @@
+﻿import time
+from datetime import datetime
+from functools import wraps
+
+def log_decorator(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        # Время начала выполнения
+        start_time = time.time()
+        start_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        
+        # Запись о вызове функции
+        with open('log.txt', 'a', encoding='utf-8') as log_file:
+            log_file.write(f"[{start_datetime}] Функция '{func.__name__}' вызвана с аргументами: {args}\n")
+        
+        # Вызов исходной функции
+        result = func(*args, **kwargs)
+        
+        # Время завершения и выполнения
+        end_time = time.time()
+        end_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        execution_time = end_time - start_time
+        
+        # Запись о завершении функции
+        with open('log.txt', 'a', encoding='utf-8') as log_file:
+            log_file.write(f"[{end_datetime}] Функция '{func.__name__}' завершена. Время выполнения: {execution_time:.4f} сек.\n")
+        
+        return result
+    return wrapper
+
+@log_decorator
+def calculate(a, b, operation):
+    if operation == '+':
+        return a + b
+    elif operation == '-':
+        return a - b
+    elif operation == '*':
+        return a * b
+    elif operation == '/':
+        return a / b
+    else:
+        raise ValueError("Неподдерживаемая операция")
+
+print(calculate(10, 5, '+'))
+print(calculate(10, 5, '-'))
+print(calculate(10, 5, '*'))
+print(calculate(10, 5, '/'))
